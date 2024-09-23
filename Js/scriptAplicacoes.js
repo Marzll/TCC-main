651684d8fff9c5270187a14a5885fac7fe47da90
Marzll <mariiamarqs@gmail.com>
2024-09-23 13:41:57 -0300

    first commit

---
+++ Js/scriptAplicacoes.js
@@ -0,0 +1,34 @@
+
+
+//funções aparecer texto
+function apliUm(){
+    var texto_1 = document.querySelector(".texto-um");
+    if (texto_1.style.fontSize == "0px"){
+        texto_1.style.fontSize = "20px";
+    } else{
+        texto_1.style.fontSize = "0px";
+    }
+    
+
+}
+
+function apliDois(){
+    var texto_2 = document.querySelector(".texto-dois");
+    if (texto_2.style.fontSize == "0px"){
+        texto_2.style.fontSize = "20px";
+    } else{
+        texto_2.style.fontSize = "0px";
+    }
+    
+}
+
+function apliTres(){
+    var texto_3 = document.querySelector(".texto-tres");
+    if (texto_3.style.fontSize == "0px"){
+        texto_3.style.fontSize = "20px";
+    } else{
+        texto_3.style.fontSize = "0px";
+    }
+    
+}
+
